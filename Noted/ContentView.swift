341b3f2777d8e850a1ca5f7e1231216269c08d31
Rob Taylor <robert.i.taylor88@gmail.com>
2020-07-16 14:27:15 +0100

    connect to backend

--- Noted/ContentView.swift
+++ Noted/ContentView.swift
@@ -17,8 +17,57 @@ struct ContentView: View {
         ZStack(alignment: .topTrailing) {
             TextView(text: $message, textStyle: $textStyle)
                 .padding(.horizontal)
-            
+            Button(action: {
+                self.closeKeyboard()
+                self.saveNotes()
+            }) {
+                Text("Save")
+            }
+            .padding()
+        }.onAppear {
+            self.getNotes()
         }
+    }
+    
+    func getNotes() {
+        let session = URLSession.shared
+        let url = URL(string: "https://glacial-badlands-85832.herokuapp.com/notes")!
+        
+        let task = session.dataTask(with: url, completionHandler: { data, response, error in
+            
+            if let json = try? JSONSerialization.jsonObject(with: data!, options: []) {
+                let jsonDict = json as? NSDictionary
+                self.message = jsonDict?["content"] as! String
+            }
+        })
+        
+        task.resume()
+    }
+    
+    func saveNotes() {
+        let session = URLSession.shared
+        let url = URL(string: "https://glacial-badlands-85832.herokuapp.com/notes")!
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        
+        let json = [
+            "username": "robt1019",
+            "content": self.message
+        ]
+        
+        let jsonData = try! JSONSerialization.data(withJSONObject: json, options: [])
+        
+        let task = session.uploadTask(with: request, from: Data(jsonData)) { data, response, error in
+        }
+        
+        task.resume()
+    }
+    
+    func closeKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
 
